0665749ff7283dd2edb3446c96d09ff4c5916b9c
tio <sulistiopratomo@gmail.com>
2023-05-12 10:46:32 +0700

    refactor: memasukan import html terkait HtmlLoader

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 // IMPORT CSS
 import '../css/style.css';
 
+import '../index.html';
+
 // IMPORT APP
 import { App } from './app';
 
@@ -8,8 +10,6 @@ const containerMenu = document.querySelector('.navbar__nav');
 const hamburgerMenu = document.querySelector('.navbar__extra__icon--hamburger-menu');
 
 const app = new App({ containerMenu, hamburgerMenu });
-
-console.log(app);
 
 window.addEventListener('DOMContentLoaded', () => {
     app.render();
