c4903ca339176d6367686a73330ba73142e735d5
tio <sulistiopratomo@gmail.com>
2023-05-13 23:18:55 +0700

    refactor: perbaiki looping item pada bagian menu

--- src/js/component/menu/index.js
+++ src/js/component/menu/index.js
@@ -1,8 +1,9 @@
 import menus from './menu-detail';
 
 const containerCard = document.querySelector('.menu__row');
-const cards = menus.map(
-    ({ name, price, img }) => /* html */ `
+const cards = menus
+    .map(
+        ({ name, price, img }) => /* html */ `
     <div class="menu__card">
         <img src=${img} alt=${img} class="menu__card__img">
         <h3 class="menu__card__title menu__card__title--text-white">${name}</h3>
@@ -10,7 +11,8 @@ const cards = menus.map(
 
     </div>
 `
-);
+    )
+    .join(' ');
 
 export const renderMenu = () => {
     containerCard.innerHTML = cards;
